5c04f5ef51ed430e45d817501056503ef0f7aa11
rohanyadav12 <152917197+rohanyadav12@users.noreply.github.com>
2024-08-09 14:40:52 +0530

    Update Day_1.js

--- DAY_1.js/Day_1.js
+++ DAY_1.js/Day_1.js
@@ -3,19 +3,19 @@
 //Activity 3-Data Types
 //Create variables of different data types (number,strings,boolean,object,array) and log each variable's type using the typeof 
 /*Task 4*/
-// let num = 5;
-// let str = "greate power comes with greate responsinlity";
-// let arr = [1,2,3,4,5];
-// let account_id = 1210432268;
-// let car = {
-//     name:"BMW",
-//     model:"m5"
-// }
-// console.log(num);
-// console.log(str);
-// console.log(arr);
-// console.log(account_id);
-// console.log(car);
+let num = 5;
+let str = "greate power comes with greate responsinlity";
+let arr = [1,2,3,4,5];
+let account_id = 1210432268;
+let car = {
+    name:"BMW",
+    model:"m5"
+}
+console.log(num);
+console.log(str);
+console.log(arr);
+console.log(account_id);
+console.log(car);
 
 //Activity 4-Reassigning Variables
 //Declare a variable using let, assign it an initial value, reassign a new value, and both values to be console.
